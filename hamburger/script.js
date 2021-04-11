c6b922234e93518a21c9576b312aa08f8c80ec41
leee1846 <leee1846@gmail.com>
2021-04-11 20:50:48 +0900

    feature: toggle 액션 적용

---
+++ hamburger/script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector("button");
+const nav = document.querySelector("nav");
+
+btn.addEventListener("click", (e) => {
+  nav.classList.toggle("active");
+  btn.classList.toggle("active");
+});
