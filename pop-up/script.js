1c6eb3f894b903d2918a99fae9906744ad7ebbed
leee1846 <leee1846@gmail.com>
2021-04-11 22:13:32 +0900

    feature: 모달창 구현

--- pop-up/script.js
+++ pop-up/script.js
@@ -1,1 +1,23 @@
+const modalBtn = document.querySelector(".btn");
+const modal = document.querySelector(".container");
+const closeBtn = document.querySelector(".close");
+const modalText = document.querySelector(".text");
 
+modalBtn.addEventListener("click", (e) => {
+  modal.style.display = "inherit";
+});
+
+closeBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (
+    e.target.className !== "modal" &&
+    e.target.parentNode.className !== "modal"
+  ) {
+    modal.style.display = "none";
+  }
+});
