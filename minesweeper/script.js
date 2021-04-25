6fc8827a0e2661293f25495eb4ead24e32d15ad2
leee1846 <leee1846@gmail.com>
2021-04-25 20:28:57 +0900

    feature: 각 칸마다 랜덤x,y값과 mine결과값 받기

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -1,3 +1,5 @@
+const board_Size = 10;
+const number_Of_Mines = 2;
 const tile_dataStatus = {
   hidden: "hidden",
   mine: "mine",
@@ -5,8 +7,36 @@ const tile_dataStatus = {
   marked: "marked",
 };
 
+//0부터 가로 갯수보다 적은 숫자만큼의 랜덤숫자만드는 함수
+function randomNumber(size) {
+  return Math.floor(Math.random() * size);
+}
+
+function positionMatch(a, b) {
+  return a.x === b.x && a.y === b.y;
+}
+
+//중복되지 않는 랜덤으로 x,y값 array만드는 함수
+function getMinePositions(boardSize, numberOfMines) {
+  const positions = [];
+
+  while (positions.length < numberOfMines) {
+    const position = {
+      x: randomNumber(boardSize),
+      y: randomNumber(boardSize),
+    };
+
+    if (!positions.some(positionMatch.bind(null, position))) {
+      positions.push(position);
+    }
+  }
+
+  return positions;
+}
+
 const createBoard = (boardSize, numberOfMines) => {
   const board = [];
+  const minePositions = getMinePositions(boardSize, numberOfMines);
 
   for (let x = 0; x < boardSize; x++) {
     const row = [];
@@ -18,6 +48,13 @@ const createBoard = (boardSize, numberOfMines) => {
         element,
         x,
         y,
+        mine: minePositions.some(positionMatch.bind(null, { x, y })),
+        get status() {
+          return this.element.dataset.status;
+        },
+        set status(value) {
+          this.element.dataset.status = value;
+        },
       };
 
       row.push(tile);
@@ -28,8 +65,7 @@ const createBoard = (boardSize, numberOfMines) => {
   return board;
 };
 
-const board_Size = 10;
-const number_Of_Mines = 2;
+//minesweeper ui화면에 구현
 const board = createBoard(board_Size, number_Of_Mines);
 const boardElement = document.querySelector(".board");
 boardElement.style.setProperty("--size", board_Size);
@@ -39,3 +75,4 @@ board.forEach((row) => {
     boardElement.append(tile.element);
   });
 });
+console.log(board);
